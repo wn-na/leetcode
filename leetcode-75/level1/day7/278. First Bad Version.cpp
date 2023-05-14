d9206b223ecea48d05c5b788e3b68cf1a0c9fe29
Lethe <37437842+0xlethe@users.noreply.github.com>
2023-05-14 11:28:44 +0900

    feat: day7 / 278. First Bad Version.cpp

---
+++ leetcode-75/level1/day7/278. First Bad Version.cpp
@@ -0,0 +1,19 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        while (left <= right) {
+            int mid = ((long)right + left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
